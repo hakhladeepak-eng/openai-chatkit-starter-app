bb2d06e22d6444b591f31af3825b7421a3e55c33
hakhladeepak-eng <hakhladeepak@gmail.com>
2025-11-06 03:33:17 +0530

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  reactStrictMode: false,
+  webpack: (config, { dev }) => {
     config.resolve.alias = {
       ...(config.resolve.alias ?? {}),
     };
+
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+
     return config;
   },
 };
